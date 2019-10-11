79b493720e27dddf6b20c86bb75ac812e547d04b
David Cervera <dmarticervera@gmail.com>
2019-10-11 11:10:17 +0200

    Unity Project creation
    Created Unity Project
    Added Main Ship (Movement + basic shooting)

---
+++ SpaceShooter/Assets/Scripts/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{   
+    //Public
+    public Vector2 m_Direction;
+    public float m_Speed;
+
+    //Private
+    private Rigidbody2D m_Rigidbody2d;
+    private Renderer m_Renderer;
+    private Camera m_MainCamera;
+
+    void Start()
+    {
+        m_Rigidbody2d = GetComponent<Rigidbody2D>();
+        m_Renderer = GetComponent<Renderer>();
+        m_MainCamera = Camera.main;
+
+        m_Rigidbody2d.gravityScale = 0.0f;
+        m_Rigidbody2d.velocity = m_Direction * m_Speed;
+    }
+
+    void Update()
+    {
+        CheckForDestroy();
+    }
+
+    void CheckForDestroy()
+    {
+        Vector3 viewPosition = m_MainCamera.WorldToViewportPoint(transform.position);
+        if (viewPosition.x > 1.0f || viewPosition.x < 0.0f || viewPosition.y > 1.0f || viewPosition.y < 0.0f)
+        {
+            Destroy(gameObject);
+        }
+        Debug.Log(viewPosition);
+    }
+}
---
+++ SpaceShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Public
+    public float m_Speed;
+    public float m_BasicBulletOffset;
+    public float m_TimeBetweenBasicShoots;
+    public GameObject m_BasicBullet;
+
+    //Private
+    private Rigidbody2D m_Rigidbody2d;
+    private float m_CurrenTime = 0.0f;
+
+
+    void Start()
+    {
+        m_Rigidbody2d = GetComponent<Rigidbody2D>();
+        m_Rigidbody2d.gravityScale = 0.0f;
+    }
+
+    void Update()
+    {
+        Move();
+        UpdateTimers();
+        ProcessInput();
+    }
+
+    void Move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        m_Rigidbody2d.AddForce(movement * m_Speed);
+    }
+
+    void UpdateTimers()
+    {
+        m_CurrenTime += Time.deltaTime;
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            ShootBasicProjectile();
+            return;
+        }
+        if(Input.GetButton("Fire2"))
+        {
+            ShootPoweredProjectile();
+            return;
+        }
+    }
+
+    void ShootBasicProjectile()
+    {
+        if(m_CurrenTime >= m_TimeBetweenBasicShoots)
+        {
+            Vector3 bulletPosition = new Vector3(transform.position.x + m_BasicBulletOffset, transform.position.y, transform.position.z);
+            Instantiate(m_BasicBullet, transform.position, Quaternion.identity);
+            m_CurrenTime = 0.0f;
+        }
+    }
+
+    void ShootPoweredProjectile()
+    {
+        //TODO
+    }
+}
